e2112de5f59f6bcc31b2a7b7e64c663987838699
Santino <marzanosantino@gmail.com>
2023-09-16 12:13:14 -0300

    Este es el main y provider

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
-
+# 9. Create Ubuntu server
+resource "aws_instance" "web-server-instance" {
+  ami               = "ami-053b0d53c279acc90"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  security_groups = ["sg-0cf654d02539ee8af"]
+  key_name          = "main-key"
+}
--- provider.tf
+++ provider.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIASLIXHCGRSIR3N274"
+  secret_key = "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"
+}
+
 
